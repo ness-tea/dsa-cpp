7a5b469042bd3948cd10d1b00c9a1f17994c8410
Vanessa Truong <v.truong9710@outlook.com>
2022-08-30 23:59:59 -0400

    Update ArrayList

--- ArrayList.cpp
+++ ArrayList.cpp
@@ -9,12 +9,14 @@ class ArrayList
 {
 private:
     int m_length = MAX_SIZE_DEFAULT;
+    int curIndex = 0;
     int* m_pArray = nullptr;
 
     void Resize()
     {
+        m_length *= 2;
         int* oldArray = m_pArray;
-        int* newArray = new int[m_length * 2];
+        int* newArray = new int[m_length];
 
         for (int i = 0; i < m_length; i++)
         {
@@ -23,6 +25,8 @@ private:
 
         delete oldArray;
         m_pArray = newArray;
+
+        cout << "Resized array to size " << m_length << endl;
     }
 
 public:
@@ -40,7 +44,13 @@ public:
 
     void Add(int entry)
     {
-        if ()
+        if (curIndex == m_length)
+        {
+            Resize();
+        }
+
+        m_pArray[curIndex] = entry;
+        curIndex++;
     }
 };
 
@@ -48,7 +58,11 @@ int main()
 {
     ArrayList arrayList = ArrayList();
 
-
+    for (int i = 0; i < 50; i++)
+    {
+        arrayList.Add(i);
+        cout << i << endl;
+    }
 
     return 0;
 }
