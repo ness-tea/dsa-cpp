fa6f89ca971fc522b6d106b785a7235f42707765
Vanessa Truong <v.truong9710@outlook.com>
2022-09-07 01:03:20 -0400

    Update permutated palindrome

--- PalindromePermutation.cpp
+++ PalindromePermutation.cpp
@@ -2,49 +2,80 @@
 #include <iostream>
 using namespace std;
 
-bool IsPermutation(string input1, string input2)
+bool useBitVector = true;
+
+bool IsPermutatedPalindrome2(string input)
 {
-    if (input1.length() != input2.length())
-    {
-        return false;
-    }
-
-    unordered_map<char, int> charCount;
-    int strLength = input1.length();
+    int charBits = 0;
+    int strLength = input.length();
 
     for (int i = 0; i < strLength; i++)
     {
-        if (charCount.find(input1[i]) == charCount.end())
-        {
-            charCount.insert({input1[i], 1});
-        }
-        else
-        {
-            charCount[input1[i]] += 1;
-        }
+        int charValue = int(input[i]) - int('a');
+
+        charBits ^= 1 << charValue;
     }
 
-    for (int i = 0; i < strLength; i++)
+    int oddCharCount = 0;
+
+    for (int i = 0; i < (sizeof(int) * 8); i++)
     {
-        if (charCount.find(input2[i]) != charCount.end())
+        if ((charBits & (1 << i)) >= 1)
         {
-            charCount[input2[i]] -= 1;
-            if (charCount[input2[i]] < 0)
+            oddCharCount += 1;
+
+            if (oddCharCount > 1)
             {
                 return false;
             }
         }
-    }    
+    }
 
     return true;
 }
 
 bool IsPermutatedPalindrome(string input)
 {
-    return false;
+    unordered_map<char, int> charCount;
+    int strLength = input.length();
+
+    for (int i = 0; i < strLength; i++)
+    {
+        if (charCount.find(input[i]) == charCount.end())
+        {
+            charCount.insert({input[i], 1});
+        }
+        else
+        {
+            charCount[input[i]] += 1;
+        }
+    }
+
+    int oddCharCount = 0;
+    for (auto it = charCount.begin(); it != charCount.end(); it++)
+    {
+        if (it->second % 2 == 1)
+        {   
+            oddCharCount += 1;
+
+            if (oddCharCount > 1)
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
 
 int main()
 {
+    string input;
+    cout << "Enter a string: ";
+    cin >> input;
+    
+    bool isPermutatedPalindrome = useBitVector ? IsPermutatedPalindrome2(input) : IsPermutatedPalindrome(input);
+    cout << "Is " << input << " a permutated palindrome? --- " << isPermutatedPalindrome << endl;
+
     return 0;
 }
