0ae7c972693c6b6eb7527272f200bf67aa24c7b8
Vanessa Truong <v.truong9710@outlook.com>
2022-10-30 21:40:30 -0400

    Add SumLists2().

--- PracticeProblems/SumLists.cpp
+++ PracticeProblems/SumLists.cpp
@@ -37,6 +37,44 @@ LinkedList<int> SumLists(LinkedList<int> l1, LinkedList<int> l2)
     return result;
 }
 
+// Digits stored in forward order
+LinkedList<int> SumLists2(LinkedList<int> l1, LinkedList<int> l2)
+{   
+    LinkedList<int> result = LinkedList<int>();
+    
+    Node<int>* pL1 = l1.m_pHead;
+    Node<int>* pL2 = l2.m_pHead;
+    Node<int>* pPrev;
+
+    while (pL1 != nullptr || pL2 != nullptr)
+    {
+        int sum = 0;
+        int carry = 0;
+        
+        if (pL1 != nullptr) sum += pL1->Data;
+        if (pL2 != nullptr) sum += pL2->Data;
+
+        if (sum >= 10)
+        {
+            sum %= 10;
+            carry++;
+        }
+
+        pPrev = result.m_pTail;
+        result.CreateNode(sum);
+
+        if (pL1 != nullptr) pL1 = pL1->Next;
+        if (pL2 != nullptr) pL2 = pL2->Next;
+
+        if (carry >=1)
+        {
+            pPrev->Data += carry;
+        }
+    }
+    
+    return result;
+}
+
 int main()
 {
     LinkedList<int> l1 = LinkedList<int>();
@@ -68,7 +106,7 @@ int main()
     l1.DisplayNodes();
     l2.DisplayNodes();
 
-    LinkedList<int> result = SumLists(l1, l2);
+    LinkedList<int> result = SumLists2(l1, l2);
 
     result.DisplayNodes();
 
